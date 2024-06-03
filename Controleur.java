aca1e61eb45c836fc7117380902d67e06a43ef29
Ted Herambert <tedherambert2004@gmail.com>
2024-06-03 11:00:24 +0200

    panel table ville

--- Controleur.java
+++ Controleur.java
@@ -1,7 +1,12 @@
 package reseau_routier;
 
 import reseau_routier.metier.Metier;
+
+import java.util.List;
+
 import reseau_routier.ihm.IHMGUI;
+import reseau_routier.metier.Ville;
+import reseau_routier.metier.Route;
 
 public class Controleur 
 {	
@@ -12,6 +17,45 @@ public class Controleur
 	{
 		this.metier = new Metier ();
 		this.ihm    = new IHMGUI (this);
+	}
+
+	// Accesseurs
+	public List<Route> getRoutes() { return this.metier.getRoutes(); }
+	public List<Ville> getVilles() { return this.metier.getVilles(); }
+
+	// Modificateurs (ignorez ça pour le moment)
+	//public boolean majPremiumClient ( int ligne, boolean val ) { return this.metier.majPremiumClient ( ligne, val ); }
+	//public boolean majAnneeAdhesion ( int ligne, int     val ) { return this.metier.majAnneeAdhesion ( ligne, val ); }
+
+	// Autres Méthodes
+	public void sauvegarder()
+	{
+		//this.metier.sauvegarder();
+	}
+	
+	public void creerVille()
+	{
+		//new FrameNouvelleVille(this);
+	}
+
+	public void creerRoute()
+	{
+		//new FrameNouvelleRoute(this);
+	}
+	
+	public void ajouterVille(Ville ville)
+	{
+		//this.metier.ajouterVille(ville);
+	}
+
+	public void ajouterRoute(Route route)
+	{
+		//this.metier.ajouterRoute(route);
+	}
+
+	public void sauvegarder()
+	{
+		//this.metier.sauvegarder();
 	}
 
 	public void deplacerLesFrames(char orig) { if(this.ihm != null) this.ihm.deplacerLesFrames(orig); }
---
+++ ihm/GrilleVillesModel.java
@@ -0,0 +1,79 @@
+package reseau_routier.ihm;
+
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+import reseau_routier.Controleur;
+import reseau_routier.metier.Ville;
+
+public class GrilleVillesModel extends AbstractTableModel
+{
+	private Controleur ctrl;
+
+	private String[]   tabEntetes;
+	private Object[][] tabVilles;
+
+	public GrilleVillesModel (Controleur ctrl)
+	{
+		this.ctrl = ctrl;
+
+		Ville clt;
+		List<Ville> lstVilles = this.ctrl.getVilles();
+
+		tabVilles = new Object[lstVilles.size()][4];
+
+		for ( int lig=0; lig<lstVilles.size(); lig++)
+		{
+			clt = lstVilles.get(lig);
+
+			tabVilles[lig][0] = clt.getPrenom  ();
+			tabVilles[lig][1] = clt.getNom     ();
+			tabVilles[lig][2] = clt.getPremium ();
+			tabVilles[lig][3] = clt.getAnneeAdh();
+		}
+
+		this.tabEntetes = new String[]   {   "Prénom"  , "Nom"     , "Premium", "Année Adhésion"  };
+
+	}
+
+	public int    getColumnCount()                 { return this.tabEntetes.length;      }
+	public int    getRowCount   ()                 { return this.tabVilles.length;      }
+	public String getColumnName (int col)          { return this.tabEntetes[col];        }
+	public Object getValueAt    (int row, int col) { return this.tabVilles[row][col];   }
+	public Class  getColumnClass(int c)            { return getValueAt(0, c).getClass(); }
+
+	public boolean isCellEditable(int row, int col)
+	{
+		return col == 2 || col == 3;
+	}
+
+	/*
+	public void setValueAt(Object value, int row, int col)
+	{
+		boolean bRet;
+
+		if ( col == 2 )
+		{
+			bRet = this.ctrl.majPremiumVille ( row, (Boolean) value );
+			if ( bRet )
+			{
+				this.tabVilles[row][col] = value;
+				this.fireTableCellUpdated(row, col);
+			}
+		}
+		
+		if ( col == 3 )
+		{
+			bRet = this.ctrl.majAnneeAdhesion ( row, (Integer) value );
+			if ( bRet )
+			{
+				this.tabVilles[row][col] = value;
+				this.fireTableCellUpdated(row, col);
+			}
+		}
+ 		
+	}
+	*/
+
+}
