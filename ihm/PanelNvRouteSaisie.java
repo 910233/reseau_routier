41da071e4f6e6e227246f0d636393ec31e6a1f8c
Rafael <debeinrafael@gmail.com>
2024-06-04 11:23:05 +0200

    Ajout de JScrollBar dans PanelNvRouteSaisie.java

--- ihm/PanelNvRouteSaisie.java
+++ ihm/PanelNvRouteSaisie.java
@@ -1,13 +1,15 @@
 package reseau_routier.ihm;
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 import reseau_routier.metier.Route;
 
-public class PanelNvRouteSaisie  extends JPanel implements Saisie
+public class PanelNvRouteSaisie  extends JPanel implements Saisie,AdjustmentListener
 {
-	private JTextField txtVilleDep, txtVilleArr, txtNbTronc;
+	private JTextField txtVilleDep, txtVilleArr;
+	private JScrollBar sbNbTronc;
 
 	public PanelNvRouteSaisie()
 	{
@@ -16,7 +18,7 @@ public class PanelNvRouteSaisie  extends JPanel implements Saisie
 		// Création des composants
 		this.txtVilleDep = new JTextField();
 		this.txtVilleArr = new JTextField();
-		this.txtNbTronc  = new JTextField();
+		this.sbNbTronc   = new JScrollBar ( JScrollBar.HORIZONTAL, 0, 1, 0, 11 );
 
 		// Positionnement des composants
 		this.add(new JLabel("Ville Dep   : "));
@@ -24,10 +26,10 @@ public class PanelNvRouteSaisie  extends JPanel implements Saisie
 		this.add(new JLabel("Ville Arr   : "));
 		this.add(this.txtVilleArr);
 		this.add(new JLabel("Nb Tronçons : "));
-		this.add(this.txtNbTronc);
+		this.add(this.sbNbTronc);
 		
 		// Activation des composants
-
+		this.sbNbTronc.addAdjustmentListener(this);
 	}
 	
 	public Route getNvRoute()
@@ -39,7 +41,7 @@ public class PanelNvRouteSaisie  extends JPanel implements Saisie
 		try {
 			villeDep = this.txtVilleDep.getText();
 			villeArr = this.txtVilleArr.getText();
-			nbTronc  = Integer.parseInt(this.txtNbTronc.getText());
+			nbTronc  = this.sbNbTronc.getValue() ;
 			//route = Route.nvRoute(villeDep, villeArr, nbTronc);
 			route = null;
 		} 
@@ -55,6 +57,11 @@ public class PanelNvRouteSaisie  extends JPanel implements Saisie
 	{
 		this.txtVilleDep.setText("");
 		this.txtVilleArr.setText("");
-		this.txtNbTronc.setText("");
+	}
+
+	public void adjustmentValueChanged ( AdjustmentEvent e )
+	{
+		if ( e.getSource() == this.sbNbTronc )
+			System.out.println(this.sbNbTronc.getValue());
 	}
 }
